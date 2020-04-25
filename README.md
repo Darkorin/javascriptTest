b2613854bcf8131f27f59034493a76dcec7c552c
David Stewart <darkorin00@hotmail.com>
2020-04-25 01:06:01 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 ##
+A javascript quiz program that I made for homework
+It is a timed multiple choice quiz that keeps track 
+of the users score and shows a high score list that
+the user can view at the end.
+
+To accomplish this I created an object to store several
+strings which I used to generate separate "pages" of 
+the quiz. Whenever a button is clicked the page gets the
+ID of the button and checks to see if it is one of the 
+"named buttons" which I reference with a unique ID. Then
+it checks to see if it is an "answer" button which I 
+reference with a dataset named response.
+
+I transition from page to page(With the exception of
+the Highscores page and a little bit of the results
+page) by simply changing the text content of my mainDiv
+and my subDiv. I then clear out my buttons and generate 
+new ones which it identifiies by the pageNumber variable.
+All the answer buttons will activate a hidden div which
+will remain visible for 1.5s showing you correct or 
+incorrect before disappearing again.
+
+The timer starts at 75 seconds and counts down. If a 
+player selects an incorrect answer the player loses 15
+seconds off of their timer. Once the player has answered all
+of their questions a results screen will pop up and show 
+their remaining time as a score with an input box to 
+input their initials. Once the submit button is clicked, 
+the player's score will be checked against the current 
+highscores(stored in local storage) and, if they beat one 
+of them, or if the list is not full they will be added to the
+list at the appropriate rank.
+
+On the highscores page you can see a list of the stored scores.
+There is a go back button which brings you back to the first page
+allowing you to try the quiz again if you want. There is also a 
+clear highscores button which removes the highscores from storage
+and rerenders the page.
---
+++ script.js
@@ -0,0 +1,258 @@
+const hishscoreDiv = document.querySelector("#highscore-div");
+const timerSpan = document.querySelector("#timer");
+const mainText = document.querySelector("#main-text");
+const subText = document.querySelector("#sub-text");
+const btnForm = document.querySelector("#button-form");
+const alertDiv = document.querySelector("#alert-div");
+const responseDiv = document.querySelector("#response-div");
+
+const page = {
+    mainText: [
+        "Coding Quiz Challenge",
+        "Commonly used data types DO NOT include:",
+        "The condition in an if/else statement is enclosed within ______.",
+        "Arrays in JavaScript can be used to store ______.",
+        "String values must be enclosed within ______ when being assigned to variables.",
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+        "All done!"],
+    subText: [
+        "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "Your final score is "],
+    btnText: [
+        ["Start Quiz"],
+        ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        ["1. paratheses", "2. curly brackets", "3. quotes", "4. square brackets"],
+        ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        ["1. commas", "2. quotes", "3. curly brackets", "paratheses"],
+        ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        ["Submit"]
+    ],
+    btnId: [
+        ["start"],
+        ["Q1A1", "Q1A2", "Q1A3", "Q1A4"],
+        ["Q2A1", "Q2A2", "Q2A3", "Q2A4"],
+        ["Q3A1", "Q3A2", "Q3A3", "Q3A4"],
+        ["Q4A1", "Q4A2", "Q4A3", "Q4A4"],
+        ["Q5A1", "Q5A2", "Q5A3", "Q5A4"],
+        ["submit"]
+    ],
+    btnResponse: [
+        [""],
+        ["Wrong!", "Wrong!", "Correct!", "Wrong!"],
+        ["Correct!", "Wrong!", "Wrong!", "Wrong!"],
+        ["Wrong!", "Wrong!", "Wrong!", "Correct!"],
+        ["Wrong!", "Correct!", "Wrong!", "Wrong!"],
+        ["Wrong!", "Wrong!", "Wrong!", "Correct!"],
+        [""]
+    ],
+}
+
+let highScores = { names: [], scores: [] };
+let storedHighScores = JSON.parse(localStorage.getItem("highscores"));
+
+if (storedHighScores !== null) {
+    highScores = storedHighScores;
+}
+
+let seconds;
+let pageNumber = 0;
+let timer;
+
+const init = function () {
+    seconds = 0;
+    pageNumber = 0;
+    mainText.removeAttribute("class");
+    mainText.removeAttribute("style");
+    subText.removeAttribute("class");
+    subText.removeAttribute("style");
+    btnForm.removeAttribute("class");
+    btnForm.removeAttribute("style");
+    mainText.setAttribute("class", "text-center");
+    mainText.setAttribute("style", "font-size: 40px; font-weight: bold;");
+    subText.setAttribute("class", "text-center");
+    mainText.setAttribute("style", "padding: 20px; font-weight: bold");
+    alertDiv.setAttribute("style", "visibility: hidden");
+    render();
+    renderTime();
+}
+const clickHandler = function (e) {
+    e.preventDefault();
+    let btnId = e.target.id;
+    let response = e.target.dataset.response;
+    let isAnswer = response === "Correct!" || response === "Wrong!";
+    if (btnId === "start") {
+        startQuiz();
+    }
+    if (response === "Wrong!") {
+        seconds -= 15;
+        renderTime();
+    }
+    if (isAnswer) {
+        transitionTimer(response);
+        nextPage();
+    }
+    if (btnId === "submit") {
+        submitScore();
+        renderHighScores();
+    }
+    if (btnId === "back") {
+        init();
+        render();
+    }
+    if (btnId === "clear") {
+        highScores = { names: [], scores: [] };
+        localStorage.setItem("highscores", JSON.stringify(highScores));
+        renderHighScores();
+    }
+
+}
+
+const startQuiz = function () {
+    seconds = 75;
+    timer = setInterval(startTimer, 1000);
+    nextPage();
+}
+
+const startTimer = function () {
+    seconds--;
+    renderTime();
+    if (seconds <= 0) {
+        clearInterval(this);
+    }
+}
+
+const renderTime = function () {
+    if (seconds < 0) {
+        seconds = 0;
+    }
+    timerSpan.textContent = seconds;
+}
+
+const nextPage = function () {
+    pageNumber++;
+    render();
+    renderTime();
+}
+
+const transitionTimer = function (response) {
+    responseDiv.textContent = response;
+    alertDiv.removeAttribute("style");
+    setTimeout(function () {
+        responseDiv.textContent = "";
+        alertDiv.setAttribute("style", "visibility: hidden");
+    }, 1500);
+}
+
+const submitScore = function () {
+    let player = document.querySelector("#initials").value;
+    if (highScores.names.length === 0) {
+        highScores.names.push(player);
+        highScores.scores.push(seconds);
+    } else {
+        for (let i = 0; i < highScores.names.length; i++) {
+            if (seconds > highScores.scores[i]) {
+                highScores.names.splice(i, 0, (player));
+                highScores.scores.splice(i, 0, (seconds));
+                if (highScores.scores.length > 10) {
+                    highScores.scores.splice(-1, 1);
+                }
+                i = 10;
+            } else if (highScores.names.length < 10) {
+                highScores.names.push(player);
+                highScores.scores.push(seconds);
+                i = 10;
+            }
+        }
+    }
+    localStorage.setItem("highscores", JSON.stringify(highScores));
+}
+
+const render = function () {
+    mainText.textContent = page.mainText[pageNumber];
+    subText.textContent = page.subText[pageNumber];
+    mainText.setAttribute("style", "font-size: 35px; font-weight: bold")
+    btnForm.innerHTML = "";
+    if (pageNumber === 0) {
+        btnForm.setAttribute("class", "text-center");
+    } else {
+        btnForm.removeAttribute("class");
+    }
+    if (pageNumber === 6) {
+        renderResults();
+    }
+    for (let i = 0; i < page.btnText[pageNumber].length; i++) {
+        let newBtn = document.createElement("button");
+        newBtn.textContent = page.btnText[pageNumber][i];
+        newBtn.setAttribute("type", "button");
+        newBtn.setAttribute("class", "btn btn-success my-1 py-1");
+        newBtn.setAttribute("id", page.btnId[pageNumber][i]);
+        newBtn.setAttribute("data-response", page.btnResponse[pageNumber][i]);
+        btnForm.append(newBtn);
+        btnForm.append(document.createElement("br"));
+    }
+}
+
+const renderResults = function () {
+    clearInterval(timer);
+    mainText.removeAttribute("class");
+    subText.removeAttribute("class");
+    subText.textContent = page.subText[pageNumber] + seconds + ".";
+    mainText.setAttribute("style", "font-size: 30px; font-weight: bold;");
+
+    let newLabel = document.createElement("label");
+    let newInput = document.createElement("input");
+    newLabel.setAttribute("for", "initials");
+    newLabel.textContent = "Enter initials:";
+    newInput.setAttribute("type", "text");
+    newInput.setAttribute("id", "initials");
+    newInput.setAttribute("name", "initials");
+    newInput.setAttribute("class", "mx-1")
+
+    btnForm.prepend(newInput);
+    btnForm.prepend(newLabel);
+}
+
+const renderHighScores = function () {
+    mainText.removeAttribute("class");
+    subText.removeAttribute("class");
+    btnForm.removeAttribute("class");
+    btnForm.innerHTML = "";
+    mainText.textContent = "Highscores";
+    mainText.setAttribute("style", "font-size: 30px; font-weight: bold; margin-left: 20px;");
+    subText.textContent = "";
+
+    for (let i = 0; i < highScores.scores.length; i++) {
+        let newScore = document.createElement("div");
+        newScore.setAttribute("class", "alert alert-success");
+        newScore.setAttribute("role", "alert");
+        newScore.textContent = `${i + 1}.${highScores.names[i]}-${highScores.scores[i]}`;
+        btnForm.append(newScore);
+    }
+
+    let backBtn = document.createElement("button");
+    let clearBtn = document.createElement("button");
+
+    backBtn.textContent = "Go Back";
+    backBtn.setAttribute("id", "back");
+    backBtn.setAttribute("type", "button");
+    backBtn.setAttribute("class", "btn btn-success m-1 p-1");
+
+    clearBtn.textContent = "Clear Highscores";
+    clearBtn.setAttribute("id", "clear");
+    clearBtn.setAttribute("type", "button");
+    clearBtn.setAttribute("class", "btn btn-success m-1 p-1");
+
+
+    btnForm.setAttribute("style", "margin-left: 20px;");
+    btnForm.append(backBtn);
+    btnForm.append(clearBtn);
+}
+
+init();
+hishscoreDiv.addEventListener("click", renderHighScores);
+btnForm.addEventListener("click", clickHandler);
